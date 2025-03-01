f76fc3bd569ffbb15e7fd12e28ce3bf73585bb48
Nicolas Riquelme <nicolas.riquelme@emasa.cl>
2025-03-01 14:40:16 -0300

    Update/hotfix add scroll function detailCharacter

--- src/app/components/character-detail/character-detail.component.ts
+++ src/app/components/character-detail/character-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
 import { SharedDataService } from 'src/app/services/shared-data.service';
 
@@ -11,7 +11,7 @@ export class CharacterDetailComponent implements OnInit {
   character: any = null;
   originResident: string | null = null;
   locationResident: string | null = null;
-
+  @ViewChild('detallePersonaje') detallePersonaje!: ElementRef;
   constructor(
     private sharedData: SharedDataService,
     private rickAndMortyService: RickAndMortyService
@@ -25,11 +25,17 @@ export class CharacterDetailComponent implements OnInit {
       }
     });
   }
-
   //Obtiene los detalles del personaje y la información adicional requerida.
   fetchCharacterDetails(characterId: number) {
     this.rickAndMortyService.getCharacterById(characterId).subscribe((data) => {
       this.character = data;
+
+            // Desplazarse después de actualizar `character`
+            if (characterId !== null && window.innerWidth <= 768) { // Solo en pantallas pequeñas
+              setTimeout(() => {
+                this.detallePersonaje?.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+              }, 200);
+            } // Pequeño delay para asegurar que el DOM se actualice
 
       // Obtener un residente del origen (si tiene origen)
       if (this.character.origin?.url) {
